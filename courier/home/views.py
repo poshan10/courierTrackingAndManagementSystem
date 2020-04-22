961b925a2eb369461e5aa712fbc0ba4ac23c992c
Prakash <prakash.subedi421@gmail.com>
2020-04-22 20:24:13 +1000

    Group wise page view permit ,, decorators created

--- courier/home/views.py
+++ courier/home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .decorators import unauthenticated_user
 
 # Create your views here.
+
+
+
 def home(request):
     return render(request, 'home.html', {'name': 'manisha'})
 
 
+@unauthenticated_user
 def index(request):
     return render(request, 'index.html', {'name': 'manisha'})
-
--- courier/login/views.py
+++ courier/login/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from .decorators import unauthenticated_user,allowed_users,admin_only
 
 # Create your views here.
 from django.urls import reverse
@@ -20,9 +22,22 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 @login_required(login_url="/login/")
+@allowed_users(allowed_roles=['Manager'])
+# @admin_only
 def success(request):
     return render(request, 'success/success.html')
+# @unauthenticated_user
 def user_logout(request):
     if request.method == "POST":
         logout(request)
         return HttpResponseRedirect(reverse('index'),)
+
+    # from django import template
+    # from django.contrib.auth.models import Group
+    #
+    # register = template.Library()
+    #
+    # @register.filter(name='has_group')
+    # def has_group(user, group_name):
+    #     group = Group.objects.get(name=group_name)
+    #     return group in user.groups.all()
